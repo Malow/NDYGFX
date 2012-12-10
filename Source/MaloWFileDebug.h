1372f8331c43858f62f5652d8c3abec147cf06b5
MaloW <malow@spray.se>
2012-12-10 15:03:47 +0100

    3D picking done

--- Source/MaloWFileDebug.h
+++ Source/MaloWFileDebug.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <fstream>
+#include "Vector.h"
 #include "MaloW.h"
 
 static bool erase = false;
@@ -52,6 +53,29 @@ namespace MaloW
 
 		writeFile.close();
 	}
+
+	inline void Debug(Vector3 v)
+	{
+		if(erase)
+		{
+			ofstream writeFile;
+			writeFile.open ("MaloWDebug.txt", ios::out | ios::trunc);
+
+			writeFile << "";
+
+			writeFile.close();
+			erase = false;
+		}
+
+
+		fstream writeFile;
+		writeFile.open ("MaloWDebug.txt", fstream::in | fstream::out | fstream::app);
+
+		writeFile << "Vector3 x: " << v.x << ", y: " << v.y << ", z: " << v.z << endl;
+
+		writeFile.close();
+	}
+
 
 	inline void ClearDebug()
 	{
